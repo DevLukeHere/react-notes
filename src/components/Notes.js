cd9a3d358ab1a466c891f18f58cf9751f08f4c26
Luke Wong <lukewong93@gmail.com>
2022-08-27 12:34:11 +0800

    Create Notes.js

---
+++ src/components/Notes.js
@@ -0,0 +1,160 @@
+import React, { useState } from "react";
+import "./Notes.css";
+
+function Notes() {
+  const [notes, setNotes] = useState([]);
+  const [activeNotes, setActiveNotes] = useState([]);
+  const [completedNotes, setCompletedNotes] = useState([]);
+  const [tab, setTab] = useState("");
+  const [name, setName] = useState("");
+  const [status, setStatus] = useState("");
+
+  const handleSubmit = () => {
+    setNotes([...notes, { name: name, status: status }]);
+    setName("");
+    setStatus("");
+  };
+
+  const handleChangeName = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleChangeStatus = (e) => {
+    setStatus(e.target.value);
+  };
+
+  const handleClickAll = () => {
+    setTab("all");
+
+    let priority = { active: 1, completed: 2 };
+
+    const sortedByPriority = notes.sort(
+      (a, b) =>
+        (priority[a.status.toLowerCase()] || Number.MAX_VALUE) -
+        (priority[b.status.toLowerCase()] || Number.MAX_VALUE)
+    );
+
+    setNotes(sortedByPriority);
+  };
+
+  const handleClickActive = () => {
+    setTab("active");
+
+    const filteredNotes = notes.filter((note) => {
+      return note.status.toLowerCase() === "active";
+    });
+
+    setActiveNotes(filteredNotes);
+  };
+
+  const handleClickCompleted = () => {
+    setTab("completed");
+
+    const filteredNotes = notes.filter((note) => {
+      return note.status.toLowerCase() === "completed";
+    });
+
+    setCompletedNotes(filteredNotes);
+  };
+
+  return (
+    <div>
+      <h1>React Notes</h1>
+
+      <div className="inputs-div">
+        <input
+          type="text"
+          data-testid="input-note-name"
+          placeholder="Note Title"
+          onChange={handleChangeName}
+          value={name}
+          className="name-input"
+        />
+        <input
+          type="text"
+          data-testid="input-note-status"
+          placeholder="Note Status"
+          onChange={handleChangeStatus}
+          value={status}
+          className="status-input"
+        />
+        <button
+          onClick={handleSubmit}
+          data-testid="submit-button"
+          className="add-note-button"
+          disabled={!name || !status ? true : false}
+        >
+          Add Note
+        </button>
+      </div>
+
+      <div className="filter-buttons">
+        <button
+          className="filter-button-all"
+          disabled={!tab || tab === "all" ? true : false}
+          onClick={handleClickAll}
+        >
+          All
+        </button>
+        <button
+          className="filter-button"
+          disabled={tab === "active" ? true : false}
+          onClick={handleClickActive}
+        >
+          Active
+        </button>
+        <button
+          className="filter-button"
+          disabled={tab === "completed" ? true : false}
+          onClick={handleClickCompleted}
+        >
+          Completed
+        </button>
+      </div>
+
+      <table className="table">
+        <thead className="table-header">
+          <tr>
+            <th>Note</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody data-testid="noteList" className="table-body">
+          {
+            tab === "active"
+              ? activeNotes.map((activeNote, index) => (
+                  <tr key={index}>
+                    <td>{activeNote.name}</td>
+                    <td>{activeNote.status}</td>
+                  </tr>
+                ))
+              : tab === "completed"
+              ? completedNotes.map((completedNote, index) => (
+                  <tr key={index}>
+                    <td>{completedNote.name}</td>
+                    <td>{completedNote.status}</td>
+                  </tr>
+                ))
+              : notes.map((note, index) => (
+                  <tr key={index}>
+                    <td>{note.name}</td>
+                    <td>{note.status}</td>
+                  </tr>
+                ))
+            // : notes
+            //     .slice(0)
+            //     .reverse()
+            //     .map((note, index) => (
+            //       <tr key={index}>
+            //         <td>{note.name}</td>
+            //         <td>{note.status}</td>
+            //       </tr>
+            //     ))
+          }
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Notes;
